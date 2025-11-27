937dfc9f33d11175a057479eebe21ed8f24809fc
Ebenezer <itzmeebenezer2007@gmail.com>
2025-11-27 09:02:46 +0530

    fix: update vite config for deployment compatibility

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import { resolve, dirname } from 'path'
+import path from 'path'
 import { fileURLToPath } from 'url'
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve:{
     alias:{
-      '@': resolve(dirname(fileURLToPath(import.meta.url)), 'src'),
-      '#components': resolve(dirname(fileURLToPath(import.meta.url)), 'src/components'),
-      '#constants': resolve(dirname(fileURLToPath(import.meta.url)), 'src/constants'),
-      '#store': resolve(dirname(fileURLToPath(import.meta.url)), 'src/store'),
-      '#hoc': resolve(dirname(fileURLToPath(import.meta.url)), 'src/hoc'),
-      '#windows': resolve(dirname(fileURLToPath(import.meta.url)), 'src/windows')
-
+      '@': path.resolve(__dirname, './src'),
+      '#components': path.resolve(__dirname, './src/components'),
+      '#constants': path.resolve(__dirname, './src/constants'),
+      '#store': path.resolve(__dirname, './src/store'),
+      '#hoc': path.resolve(__dirname, './src/hoc'),
+      '#windows': path.resolve(__dirname, './src/windows')
     }
   }
 })
